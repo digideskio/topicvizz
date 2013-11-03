6a8cdbd3293c950f8abf2e2e183caac53ec496b2
Jorge H. F. Pereira <jorge.h.f.pereira@gmail.com>
2013-11-03 22:23:23 +0100

    arbor.js durch d3.js ersetzt

--- vizz/js/main.js
+++ vizz/js/main.js
@@ -1,204 +1,163 @@
 (function($){
+	"use strict";
 
+	var vizsvg;
+	var force;
 	var info_box_open = false;
 	var current_info_data = null;
+	
 	var info_collection = [];
 
-	var Renderer = function(canvas){
-		var canvas = $(canvas).get(0)
-		var ctx = canvas.getContext("2d");
-		var particleSystem
+	var graph = {
+	  "nodes":[
+		{"id":"Myriel","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Napoleon","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Mlle.Baptistine","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Mme.Magloire","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"CountessdeLo","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Geborand","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Champtercier","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Cravatte","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Count","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"OldMan","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Labarre","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Valjean","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Marguerite","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Mme.deR","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Isabeau","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"},
+		{"id":"Gervais","color":"rgb(12,34,56)", "name": "njaa", "desc": "blaaa"}
+	  ],
+	  "links":[
+		{"source":1,"target":2,"value":1},
+		{"source":2,"target":1,"value":8},
+		{"source":3,"target":1,"value":10},
+		{"source":3,"target":2,"value":6},
+		{"source":4,"target":1,"value":1},
+		{"source":5,"target":1,"value":1},
+		{"source":6,"target":1,"value":1},
+		{"source":7,"target":1,"value":1},
+		{"source":8,"target":1,"value":2},
+		{"source":9,"target":1,"value":1}
+	  ]
+	};
 
-		var that = {
-			init:function(system){
-				//
-				// the particle system will call the init function once, right before the
-				// first frame is to be drawn. it's a good place to set up the canvas and
-				// to pass the canvas size to the particle system
-				//
-				// save a reference to the particle system for use in the .redraw() loop
-				particleSystem = system
 
-				// inform the system of the screen dimensions so it can map coords for us.
-				// if the canvas is ever resized, screenSize should be called again with
-				// the new dimensions
 
-				var win_jq = $(window);
-
-				canvas.width = win_jq.width() - 8;
-				canvas.height = win_jq.height() - 8;
-
-				particleSystem.screenSize(canvas.width, canvas.height);
-				particleSystem.screenPadding(150); // leave an extra 80px of whitespace per side
-
-				win_jq.resize(function() {
-					canvas.width = $(this).width() - 8;
-					canvas.height = $(this).height() - 8;
-					particleSystem.screenSize(canvas.width, canvas.height);
-					that.redraw();
-				});
-
-				// set up some event handlers to allow for node-dragging
-				that.initMouseHandling();
-			},
-
-			redraw:function(){
-				//
-				// redraw will be called repeatedly during the run whenever the node positions
-				// change. the new positions for the nodes can be accessed by looking at the
-				// .p attribute of a given node. however the p.x & p.y values are in the coordinates
-				// of the particle system rather than the screen. you can either map them to
-				// the screen yourself, or use the convenience iterators .eachNode (and .eachEdge)
-				// which allow you to step through the actual node objects but also pass an
-				// x,y point in the screen's coordinate system
-				//
-				//ctx.fillStyle = "white"
-				ctx.clearRect(0,0, canvas.width, canvas.height)
-
-				particleSystem.eachEdge(function(edge, pt1, pt2){
-					// edge: {source:Node, target:Node, length:#, data:{}}
-					// pt1: {x:#, y:#} source position in screen coords
-					// pt2: {x:#, y:#} target position in screen coords
-
-					// draw a line from pt1 to pt2
-					ctx.strokeStyle = "rgba(255,255,255, .333)"
-					ctx.lineWidth = 2
-					ctx.beginPath()
-					ctx.moveTo(pt1.x, pt1.y)
-					ctx.lineTo(pt2.x, pt2.y)
-					ctx.stroke()
-				});
-
-				particleSystem.eachNode(function(node, pt){
-					// node: {mass:#, p:{x,y}, name:"", data:{}}
-					// pt: {x:#, y:#} node position in screen coords
-
-					// draw a rectangle centered at pt
-					var w = 15;
-					ctx.fillStyle = (node.data.color) ? node.data.color : "white"
-					//ctx.fillRect(pt.x-w/2, pt.y-w/2, w,w)
-					ctx.beginPath();
-					var r = (node.data.root || node.data.alone) ? 5: 0;
-					//console.log(node.data.text, ctx.measureText(node.data.text))
-					ctx.arc(pt.x, pt.y, w+r+((node.data.text) ? ctx.measureText(node.data.text).width:0)/2, 0, 2*Math.PI, false);
-					ctx.fill();
-
-					if(node.data.text) {
-						ctx.fillStyle = 'white';
-						ctx.font = 'bold 15px Arial';
-						ctx.fillText(node.data.text, pt.x - ctx.measureText(node.data.text).width/2, pt.y + 5)
-					}
-				});
-			},
-
-			initMouseHandling:function(){
-				// no-nonsense drag and drop (thanks springy.js)
-				var dragged = null;
-				var dblclicked = null;
-
-				// set up a handler object that will initially listen for mousedowns then
-				// for moves and mouseups while dragging
-				var handler = {
-					clicked:function(e){
-						var pos = $(canvas).offset();
-						_mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
-						dragged = particleSystem.nearest(_mouseP);
-
-						if (dragged && dragged.node !== null){
-							// while we're dragging, don't let physics move the node
-							dragged.node.fixed = true
-						}
-
-						$(canvas).bind('mousemove', handler.dragged)
-						$(window).bind('mouseup', handler.dropped)
-
-						return false
-					},
-					dblclicked: function(e) {
-						var pos = $(canvas).offset();
-						_mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
-						dblclicked = particleSystem.nearest(_mouseP);
-
-						if (dblclicked && dblclicked.node !== null){
-							if(dblclicked.node.data.text) {
-								//console.log(dblclicked.node.data.text);
-							
-								if(!info_box_open) {
-									info_box_open = true;
-									
-									var info_box = $('#info_box');
-									
-									current_info_data = dblclicked.node.data;
-								
-									info_box.find(".head h2").html(dblclicked.node.data.text);
-								
-									if(dblclicked.node.data.desc)
-										info_box.find(".body").html(dblclicked.node.data.desc);
-								
-									info_box
-									.css({
-										opacity: 0.0,
-										display: 'block'
-									})
-									.animate({
-										opacity: 1.0,
-										width: '500px',
-										height: '400px',
-										marginLeft: '-250px',
-										marginTop: '-200px'
-									}, 300);
-								}
-							}
-						}
-
-						dblclicked = null;
-
-						return false
-						
-					},
-					dragged:function(e){
-						var pos = $(canvas).offset();
-						var s = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
-
-						if (dragged && dragged.node !== null){
-						  var p = particleSystem.fromScreen(s)
-						  dragged.node.p = p
-						}
-
-						return false
-					},
-
-					dropped:function(e){
-						if (dragged===null || dragged.node===undefined) return
-						if (dragged.node !== null) dragged.node.fixed = false
-						dragged.node.tempMass = 1000
-						dragged = null
-						$(canvas).unbind('mousemove', handler.dragged)
-						$(window).unbind('mouseup', handler.dropped)
-						_mouseP = null
-						return false
-					}
-				}
-
-				// start listening
-				$(canvas).mousedown(handler.clicked);
-				$(canvas).dblclick(handler.dblclicked);
-			}
-		}
+	function initGraph(vis) {
 		
-		return that
+		//var color = d3.scale.category20();
+		
+		var height = $(window).height(),
+			width = $(window).width();
+		
+		vizsvg.style.height = height + "px";
+		vizsvg.style.width = width + "px";
+		
+		force = d3.layout.force()
+		    .nodes(graph.nodes)
+		    .links(graph.links)
+		    .charge(-100)
+		    .gravity(0.01)
+		    .linkStrength(0.2)
+		    .linkDistance(300)
+		    .size([width,height])
+		    .start();
+		    
+		    
+			var link = vis.selectAll(".link")
+				.data(graph.links)
+				.enter().append("line")
+				.attr("class", "link")
+				.style("stroke-width", function(d) { return Math.sqrt(d.value); })
+				.style("stroke", function(d) {
+				
+					return "rgb(255, 255, 255)";
+				
+				});
+
+			var node = vis.selectAll(".node")
+				.data(graph.nodes)
+				.enter().append('g')
+				.attr("class", "node");
+				
+				node.append("circle")
+				.attr("r", 30)
+				.style("fill", function(d) {
+					
+					return d.color;
+					
+				})
+				.on('dblclick', function(d, i) {
+					if(d.name) {
+						if(!info_box_open) {
+							info_box_open = true;
+							
+							var info_box = $('#info_box');
+							
+							current_info_data = d;
+						
+							info_box.find(".head h2").html(current_info_data.name);
+						
+							if(current_info_data.desc)
+								info_box.find(".body").html(current_info_data.desc);
+						
+							info_box
+							.css({
+								opacity: 0.0,
+								display: 'block'
+							})
+							.animate({
+								opacity: 1.0,
+								width: '500px',
+								height: '400px',
+								marginLeft: '-250px',
+								marginTop: '-200px'
+							}, 300);
+						}
+					}
+				});
+				
+				node.append('text')
+				.attr("fill", "rgb(255, 255, 255)")
+				.attr("text-anchor", "middle")
+				.attr("pointer-events", "none")
+				.attr("y", 5)
+				.html(function(d, i) {
+					return d.name;
+				});
+			
+				node.call(force.drag);
+			
+			force.on("tick", function() {
+				link.attr("x1", function(d) { return d.source.x; })
+					.attr("y1", function(d) { return d.source.y; })
+					.attr("x2", function(d) { return d.target.x; })
+					.attr("y2", function(d) { return d.target.y; });
+				
+				node.attr("transform", function(d, i) {
+					return "translate(" + d.x + ", " + d.y + ")";
+				});
+				//node.attr("cx", function(d) { return d.x; })
+				//	.attr("cy", function(d) { return d.y; });
+			  });
 	}
 
-	$(document).ready(function(){
-		var canvas_id = "graph_viz";
-		var c = $("#" + canvas_id).get(0);
-		c.width = 600;
-		c.height = 600;
+	
 
-		//c.getContext('2d').mozImageSmoothingEnabled = false;
+	$(document).ready(function(){
+		
+		vizsvg = document.querySelector('#graph_viz');
+		
+		var vis = d3.select(vizsvg);
+		
+		initGraph(vis);
+		
+		//vis.addAttribute('viewBox', '0 0');
+		
+		
 
 		var time_topbar_node = $("#time_topbar"),
-			filter_sidebar_node = $("#filter_sidebar");
+			filter_sidebar_node = $("#filter_sidebar"),
 			collector_sidebar_node = $('#collector_sidebar');
 		var time_topbar_title_node = time_topbar_node.find(".title span"),
 			filter_sidebar_title_node = filter_sidebar_node.find(".title span");
@@ -210,23 +169,6 @@
 		var left_handle_value_node = time_topbar_node.find(".left_handle_value"),
 			right_handle_value_node = time_topbar_node.find(".right_handle_value");
 
-		var sys = arbor.ParticleSystem(1000, 600, 0.5); // create the system with sensible repulsion/stiffness/friction
-		sys.parameters({gravity:true}); // use center-gravity to make the graph settle nicely (ymmv)
-		sys.renderer = Renderer("#" + canvas_id); // our newly created renderer will have its .init() method called shortly by sys...
-
-		// add some nodes to the graph and watch it go...
-		sys.addNode('a', {color: 'orange', text: 'baaamm!', root: true, desc: 'Ahoy hoy!'});
-		sys.addNode('e', {color: '#05A2C4', text: 'yamm!', alone: true, desc: 'Geht!'});
-		sys.addNode('b', {color: 'red', text: 'nyan~nyan~nyan', desc: 'Cat!'});
-		sys.addEdge('a','b');
-		sys.addEdge('a','c');
-		sys.addEdge('a','d');
-
-
-		// test
-		for(var i = 1; i < 250; i++) { // 250 -> Chromium; 
-			//sys.addNode("" + i, {color: "#222", text: "" + i});
-		}
 
 
 
@@ -269,6 +211,7 @@
 			
 			new_item.fadeIn(300);
 		}
+
 
 
 
@@ -358,10 +301,11 @@
 		
 		
 		// add info box close handler
-		$("#info_box .head .close_button")
+		$("#info_box .content .head .close_button")
 		.css({opacity: 0.7})
 		.hover(
 			function() {
+				console.log("asf");
 				$(this).stop().animate({opacity: 1.0}, 100);
 			},
 			function() {
@@ -416,7 +360,7 @@
 				// add a copy to the collection if it hasn't been already added
 				for(var i = 0; i < info_collection.length; i++) {
 				
-					if(info_collection[i].desc == current_info_data.desc) // TODO: check by id, when available
+					if(info_collection[i].id == current_info_data.id) // TODO: check by id, when available
 						return;
 			
 				}
@@ -447,6 +391,18 @@
 				left_handle_value_node.html(""+ui.values[0]);
 				right_handle_value_node.html(""+ui.values[1]);
 			}
+		});
+		
+		
+		// when window is being resized
+		$(window).resize(function() {
+		
+			var height = $(window).height(),
+				width = $(window).width();
+		
+			force.size([width, height]);
+			vizsvg.style.height = height + "px";
+			vizsvg.style.width = width + "px";
 		});
 	})
 
