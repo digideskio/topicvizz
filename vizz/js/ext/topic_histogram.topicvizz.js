8517e944eecc968d12773b22acd1b3f003e051ae
Jorge H. F. Pereira <jorge.h.f.pereira@gmail.com>
2014-06-15 05:41:12 +0200

    Vergleichmodus für die Topic-Histogram-Extension eingeführt

--- vizz/js/ext/topic_histogram.topicvizz.js
+++ vizz/js/ext/topic_histogram.topicvizz.js
@@ -1,5 +1,6 @@
 
 (function($, window) {
+    "use strict";
 
     /* Informationen über die Extension */
     var ext_info = {
@@ -10,12 +11,17 @@
 
     var is_open = false;
 
-    var m_node          = null;
-    var m_svg_node      = null;
-    var m_defs_node     = null;
-    var m_data          = null;
-    var m_graph_data    = null;
-    var m_helper        = null;
+    var m_node                  = null;
+    var m_svg_node              = null;
+    var m_defs_node             = null;
+    var m_data                  = null;
+    var m_graph_data            = null;
+    var m_helper                = null;
+    var m_content_wrapper_node  = null;
+    var m_content_node          = null;
+    var m_inspector_area        = null;
+    var m_insp_topics_list      = null;
+    var m_insp_topics_diag      = null;
 
     var m_callbacks = {
         onShow: $.noop,
@@ -63,10 +69,10 @@
             });
             node.append(close_button);
                         
-            var content_wrapper = $('<div>').addClass('content_wrapper')
+            m_content_wrapper_node = $('<div>').addClass('content_wrapper')
                                             .addClass('nano');
 
-                var content = $('<div>').addClass('content');
+                m_content_node = $('<div>').addClass('content');
                     
                     var svg = $(document.createElementNS('http://www.w3.org/2000/svg', 'svg'));
                     svg.attr("xmlns:xlink", "http://www.w3.org/1999/xlink");
@@ -74,13 +80,51 @@
                         m_defs_node = $(document.createElementNS('http://www.w3.org/2000/svg', 'defs'));
                         svg.prepend(m_defs_node);
                     
-                    content.append(svg);
+                    m_content_node.append(svg);
                     m_svg_node = svg;
                     
-                content_wrapper.append(content);
+                                     
+                    m_content_node.on('scroll', function(e) {
+                        if(m_content_node.data('timeoutHandler'))
+                            window.clearTimeout(m_content_node.data('timeoutHandler'));
+                        
+                        m_content_node.addClass('scrolling');
+                        
+                        var timeoutHandler = window.setTimeout(function() {
+                            m_content_node.removeClass('scrolling').data('timeoutHandler', null);
+                        }, 200);
+                        
+                        m_content_node.data('timeoutHandler', timeoutHandler);
+                    });
+                
+                m_content_wrapper_node.append(m_content_node);
 
-            node.append(content_wrapper);
-
+            node.append(m_content_wrapper_node);
+            
+            /* Bereich für die Übereinanderlegung von Diagrammen; höchstens vier Topics */
+            m_inspector_area = $('<div>').addClass('inspector_area');
+            m_inspector_area.hide();
+            
+                m_insp_topics_list = $('<ul>').addClass('insp_topic_list');
+                
+                var topic_list_wrapper = $('<div>').css({width: '200px', float: 'left'});
+                topic_list_wrapper.append(m_insp_topics_list);
+                
+                m_inspector_area.append(topic_list_wrapper);
+                
+                var svg_elem = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+                m_insp_topics_diag = $(svg_elem).attr('class', 'inspector_histogram_holder');
+                m_insp_topics_diag.attr({width: '700px', height: '170px'});
+                
+                var topic_diagram_con = $('<div>').css({width: '700px', float: 'right'});
+                topic_diagram_con.append(m_insp_topics_diag);
+                
+                m_inspector_area.append(topic_diagram_con);
+                
+                m_inspector_area.append($('<div>').css('clear', 'both'));
+            
+            node.append(m_inspector_area);
+            
 
             var sub_bar = $('<div>').addClass('sub_bar');
             
@@ -161,7 +205,7 @@
                 var histogramvizsvg_jq = m_svg_node;
                 
                 /* Node-Referenz des DIV-Elements holen */
-                histogramvizsvg = histogramvizsvg_jq.get(0);
+                var histogramvizsvg = histogramvizsvg_jq.get(0);
                 
                 var histogramvis = d3.select(histogramvizsvg);
 
@@ -185,7 +229,7 @@
                 /* Gruppe für den Histogrammpfad erzeugen */
                 item_g.append("g")
                     .attr("class", "histogram_path")
-                    .attr("transform", function(d, i) { return "translate(100, " + ((i + 1) * 30) + ")"; })
+                    .attr("transform", function(d, i) { return "translate(120, " + ((i + 1) * 30) + ")"; })
                     .append("path")
                     .attr("d", function(d, i) {
                         
@@ -204,7 +248,8 @@
                         
                         return m_helper.line_function(data_arr, 850, 50, m_histogram_min_max.max);
                     })
-                    .attr("stroke-width", "0");
+                    .attr("stroke-width", 0);
+                
                 
                 var g_nodes = $(histogramvis.node()).children().not('defs');
                 
@@ -229,7 +274,7 @@
                     var g_node_clone = g_year_node.clone();
                     g_node_clone.attr("transform", function(d) {
                         new_height = (i + 1) * 140 + 20;
-                        return "translate(115, " + new_height + ")";
+                        return "translate(135, " + new_height + ")";
                     });
                     
                     $(d).after(g_node_clone);
@@ -237,7 +282,6 @@
                 
                 item_g.append("text")
                     .attr("class", "item_title_text")
-                    .attr("pointer-events", "none")
                     .attr("dx", "20px")
                     .attr("y", function(d, i) {
                         return ((i + 1) * 30) + "px";
@@ -245,6 +289,168 @@
                     .html(function(d, i) {
                         /* Topic-Name als Inhalt des Text-Elements setzen */
                         return d.topic;
+                    })
+                    .on('click', function(d, i) {
+                        
+                        var list_cnt = m_insp_topics_list.children().length;
+                        var color_pos = list_cnt;                        
+                        var class_number = ['one', 'two', 'three', 'four', 'five'][list_cnt];
+                        
+                        var svg_elem_d3 = d3.select(m_insp_topics_diag.get(0));
+                        var data_arr = [];
+                        
+                        
+                        var active_topics = m_insp_topics_list.data('active_topics');
+                        if(!active_topics)
+                            active_topics = {};
+                        
+                        if(list_cnt < 5 && !active_topics[d.topic]) {
+                            
+                            /* 
+                             * Farben von Topic wiederverwenden, die evtl. zuvor gelöscht wurden,
+                             *  um mehreren Topics nicht die slebe Farbe zu zuweisen
+                             */
+                            var given_up_color_pos = m_insp_topics_list.data('given_up_color_pos');
+                            if(given_up_color_pos && given_up_color_pos.length > 0) {
+                                color_pos = given_up_color_pos.shift();
+                                class_number = ['one', 'two', 'three', 'four', 'five'][color_pos];
+                            }
+                            
+                            var topic_list_entry_remove = $('<span>')
+                                .html(' x ')
+                                .addClass('close')
+                                .data('color_pos', color_pos)
+                                .on('click', function() {
+                                    var path_node = m_insp_topics_diag.find('.' + class_number);
+
+                                    var path_elem_cnt = m_insp_topics_list.children().length;
+                                    
+                                    if(path_elem_cnt === 1) {
+                                        /* Inspektionsbereich schließen, wenn das letzte Topic aus dem Inspektionsbereich entfernt wird */
+                                        var prev_height = m_content_wrapper_node.data('prev_height');
+                                        
+                                        m_inspector_area.fadeOut();
+                                        m_inspector_area.animate({ height: '0px' }, 400, function() {
+                                            m_content_wrapper_node.animate({ height: prev_height + 'px' }, 400, function() {
+                                                m_content_wrapper_node.nanoScroller();
+                                            });
+                                        });
+                                    }
+                                    else {
+                                        var enabled_path_elem_cnt = m_insp_topics_list.children().not('.disabled').length;
+                                        d3.select(path_node.get(0)).style('opacity', 1.0 / (enabled_path_elem_cnt - 1));
+                                    }
+                                    
+                                    var active_topics = m_insp_topics_list.data('active_topics');
+                                    active_topics[d.topic] = false;
+                                    m_insp_topics_list.data('active_topics', active_topics);
+
+                                    var given_up_color_pos = m_insp_topics_list.data('given_up_color_pos');
+                                    if(!given_up_color_pos)
+                                        given_up_color_pos = [];
+                                    
+                                    given_up_color_pos.push($(this).data('color_pos'));
+                                    
+                                    m_insp_topics_list.data('given_up_color_pos', given_up_color_pos);
+                                    
+                                    $(this).parent().remove();
+                                    path_node.remove();
+                                });
+                            
+                            
+                            var topic_list_entry_title = $('<span>')
+                                .html(d.topic)
+                                .addClass('entry')
+                                .addClass(class_number)
+                                .on('mouseover', function() {
+                                    var path_node = m_insp_topics_diag.find('.' + class_number);
+                                    path_node.parent().parent().append(path_node.parent());
+                                    d3.select(path_node.get(0)).transition().style('opacity', 1.0);
+                                })
+                                .on('mouseleave', function() {
+                                    var path_node = m_insp_topics_diag.find('.' + class_number);
+                                    var enabled_path_elem_cnt = m_insp_topics_list.children().not('.disabled').length;
+                                    d3.select(path_node.get(0)).transition().style('opacity', 1.0 / enabled_path_elem_cnt);
+                                })
+                                .on('click', function() {
+                                    var active_status = topic_list_entry.data('active');
+                                    var path_node_d3 = d3.select(m_insp_topics_diag.find('.' + class_number).get(0));
+                                    var visibility = 'visible';
+                                    
+                                    if(active_status) {
+                                        visibility = 'hidden';
+                                        $(this).css('color', '#fff').parent().addClass('disabled');
+                                        active_status = false;
+                                    }
+                                    else {
+                                        active_status = true;
+                                        $(this).css('color', '').parent().removeClass('disabled');
+                                    }
+                                    
+                                    path_node_d3.style('visibility', visibility);
+                                    topic_list_entry.data('active', active_status);
+                                    
+                                    var enabled_path_elem_cnt = m_insp_topics_list.children().not('.disabled').length;
+                                    svg_elem_d3.selectAll('path').transition().style('opacity', 1.0 / enabled_path_elem_cnt);
+                                });
+                            
+                            var topic_list_entry = $('<li>')
+                                .data('active', true)
+                                .append(topic_list_entry_remove)
+                                .append(topic_list_entry_title);
+                            
+                            
+                            m_insp_topics_list.append(topic_list_entry);
+                            active_topics[d.topic] = true;
+                            m_insp_topics_list.data('active_topics', active_topics);
+                            
+                            var enabled_path_elem_cnt = m_insp_topics_list.children().not('.disabled').length;
+                            
+                            svg_elem_d3.append('g')
+                                .attr('class', 'histogram')
+                                .attr('transform', 'translate(0, 20)')
+                                .append('path')
+                                .attr('class', class_number)
+                                .attr('d', function() {
+                                    var histogram_arr = d.frequency_per_year;
+                                    
+                                    /* Jahresdaten für die Generierung des Histogrammpfads */
+                                    data_arr = [];
+                                    for(var i = m_years_min_max.min; i <= m_years_min_max.max; i++) {
+                                        var val = histogram_arr[''+i];
+                                        
+                                        if(typeof(val) === 'undefined')
+                                            val = 0;
+                                        
+                                        data_arr.push(val);
+                                    }
+                                    
+                                    return m_helper.line_function(data_arr, 700, 60, m_histogram_min_max.max);
+                                })
+                                .style('opacity', 1.0 / (enabled_path_elem_cnt + 1));
+                            
+                            /* Pfade eine gewisse Transparenz geben, um übereinanderliegende Pfade besser unterscheiden zu können */
+                            svg_elem_d3.selectAll('path').transition().style('opacity', 1.0 / enabled_path_elem_cnt);
+                        }
+                        
+                        if(list_cnt === 0) {
+                            /* Inspektionsbereich öffnen und Histogrammauflsitung verkleinern */
+                            m_content_wrapper_node.data('prev_height', m_content_wrapper_node.height());
+                            
+                            m_content_wrapper_node.animate({ height: '400px' }, 400, function() {
+                                m_inspector_area.fadeIn();
+                                m_inspector_area.animate({ height: '170px' }, 400);
+                                
+                                m_content_wrapper_node.nanoScroller();
+                            });
+                            
+                            /* Zeitachse erzeugen */
+                            var time_axis_g_node = svg_elem_d3
+                                                        .append('g')
+                                                        .attr('transform', 'translate(15, 140)');
+                            
+                            m_helper.create_year_text_in_group(time_axis_g_node, m_years_min_max.min, data_arr.length, 700);
+                        }
                     });
                 
                 
@@ -266,7 +472,18 @@
             m_callbacks.onHide();
             
             /* Popup bzw. Overlay ausblenden */
-            m_node && m_node.stop().fadeOut(function() { m_svg_node.empty(); });
+            m_node && m_node.stop().fadeOut(function() {
+                m_svg_node.empty();
+                
+                var prev_height = m_content_wrapper_node.data('prev_height');
+                m_content_wrapper_node.css({ height: prev_height + 'px' }, 400);
+                m_inspector_area.fadeOut();
+                m_inspector_area.css({ height: '0px' });
+                
+                m_insp_topics_diag.empty();
+                m_insp_topics_list.empty();
+                m_insp_topics_list.data('active_topics', {});
+            });
             
             /* Nano-Scrollbar zerstören */
             m_node.find(".nano").nanoScroller({ stop: true });
